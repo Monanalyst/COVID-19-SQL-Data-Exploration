f5d240e423c370c8dc1e822a5800df2680dd7049
Monanalyst <123024674+Monanalyst@users.noreply.github.com>
2023-07-07 01:06:55 +0100

    Add files via upload

---
+++ COVID Portfolio Project - Data Exploration.sql
@@ -0,0 +1,204 @@
+SELECT *
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL 
+ORDER BY 3,4
+
+--SELECT *
+--FROM PortfolioProject.dbo.CovidVaccinations
+
+-- Select Data that we going to be using
+
+SELECT Location, date, total_cases, new_cases, total_deaths, population
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2
+
+-- Looking at Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract COVID in your country
+
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE location = 'United Kingdom' AND continent IS NOT NULL
+ORDER BY 1,2
+
+-- Looking at Total Cases vs Population
+-- Shows what perentage of population got COVID
+
+SELECT Location, date, population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE location = 'United Kingdom'
+AND continent IS NOT NULL
+ORDER BY 1,2
+
+-- Looking at Countries with Highest Infection Rate compared to Population
+
+SELECT Location, population, MAX(total_cases) AS HighestInfectionCount
+, MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM PortfolioProject.dbo.CovidDeaths
+GROUP BY population, location
+ORDER BY PercentPopulationInfected DESC
+
+-- Showing Countries with Highest Death Count per Population
+/* By checking/identifying the total_deaths column, the data type was a varchar which provided incorrect values. 
+So in order to convert TotalDeathCount to an integar, the CAST function was used */ 
+
+SELECT Location, MAX(CAST(total_deaths AS int)) AS TotalDeathCount
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY TotalDeathCount DESC
+
+-- LET'S BREAK THINGS DOWN BY CONTINENT
+-- Showing Continents with the Highest Death Count per Population
+
+SELECT continent, MAX(CAST(total_deaths AS int)) AS TotalDeathCount
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
+
+-- GLOBAL NUMBERS PER DAY
+
+SELECT date, SUM(new_cases) AS total_new_cases, SUM(CAST(new_deaths AS int)) AS total_new_deaths
+, SUM(CAST(new_deaths AS int))/SUM(new_cases)*100 AS DeathPercentage
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1,2
+
+SELECT date, SUM(total_cases) AS total_cases, SUM(CAST(total_deaths AS int)) AS total_deaths
+, SUM(CAST(total_deaths AS int))/SUM(total_cases)*100 AS DeathPercentage
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1,2
+
+-- GLOBAL NUMBERS AS A WHOLE
+
+SELECT SUM(new_cases) AS new_cases, SUM(CAST(new_deaths AS int)) AS new_deaths
+, SUM(CAST(new_deaths AS int))/SUM(new_cases)*100 AS DeathPercentage
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2
+
+SELECT SUM(total_cases) AS total_cases, SUM(CAST(total_deaths AS int)) AS total_deaths
+, SUM(CAST(total_deaths AS int))/SUM(total_cases)*100 AS DeathPercentage
+FROM PortfolioProject.dbo.CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2
+
+-- Looking at Total Population vs Vaccinations
+-- Shows percentage of population that has recieved at aleast one Covid vaccine
+
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations AS int)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+FROM PortfolioProject.dbo.CovidDeaths dea
+JOIN PortfolioProject.dbo.CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3
+
+-- Using CTE to perform calculation on Partition By in previous query because you cannot just use the alias column created in the aggregate function in the above SELECT statement
+-- Shows the percentage of people vaccinated in a given location on a day-by-day basis
+
+WITH PopvsVac (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
+AS
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations AS int)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+FROM PortfolioProject.dbo.CovidDeaths dea
+JOIN PortfolioProject.dbo.CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+)
+SELECT *, (RollingPeopleVaccinated/population)*100 AS PercPopVaccinated
+FROM PopvsVac
+
+-- Shows the Total/Maximum percentage of people vaccinated per country
+
+WITH PopvsVac (continent, location, population, new_vaccinations, RollingPeopleVaccinated)
+AS
+(
+SELECT dea.continent, dea.location, dea.population, vac.new_vaccinations
+, SUM(CAST(new_vaccinations AS int)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM PortfolioProject.dbo.CovidDeaths dea
+JOIN PortfolioProject.dbo.CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+)
+SELECT continent, location, population, MAX((RollingPeopleVaccinated)/population)*100 AS MaxVaccinations
+FROM PopvsVac
+GROUP BY continent, location, population
+ORDER BY location
+
+-- TEMP TABLE (Alternate method to previous CTE)
+-- Using the DROP TABLE function allows to make changes within the the temp table without any errors when running the query
+
+DROP TABLE if exists #PercentagePopulationVaccinated
+CREATE TABLE #PercentPopulationVaccinated
+(
+continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+INSERT INTO #PercentPopulationVaccinated
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations AS int)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+FROM PortfolioProject.dbo.CovidDeaths dea
+JOIN PortfolioProject.dbo.CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+
+SELECT *, (RollingPeopleVaccinated/population)*100 AS PercPopVaccinated
+FROM #PercentPopulationVaccinated
+
+-- TEMP TABLE (Alternate method to previous CTE)
+
+DROP TABLE if exists #MaxPercentPopulationVaccinated
+CREATE TABLE #MaxPercentPopulationVaccinated
+(
+continent nvarchar(255),
+location nvarchar(255),
+population numeric,
+new_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+INSERT INTO #MaxPercentPopulationVaccinated
+SELECT dea.continent, dea.location, dea.population, vac.new_vaccinations
+, SUM(CAST(new_vaccinations AS int)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM PortfolioProject.dbo.CovidDeaths dea
+JOIN PortfolioProject.dbo.CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+
+SELECT continent, location, population, MAX((RollingPeopleVaccinated)/population)*100 AS MaxVaccinations
+FROM #MaxPercentPopulationVaccinated
+GROUP BY continent, location, population
+ORDER BY location
+
+-- Creating View to store data for later visualisations
+
+CREATE VIEW PercentPopulationVaccinated AS
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations AS int)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+FROM PortfolioProject.dbo.CovidDeaths dea
+JOIN PortfolioProject.dbo.CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
